71aed53c919ba789d9af7cf709f87b1fd636e540
John Brady <bradyjoh@grinnell.edu>
2015-02-22 09:42:28 -0600

    Initial Commit

---
+++ client.c
@@ -0,0 +1,6 @@
+/* Harry Baker & John Brady
+CSC214, Stone
+HW2 - Client-Server protocol
+February 24, 2015
+client.c - contains client-side protocols
+*/
---
+++ server.c
@@ -0,0 +1,6 @@
+/* Harry Baker & John Brady
+CSC214, Stone
+HW2 - Client-Server protocol
+February 24, 2015
+server.c - contains server-side protocols
+*/
